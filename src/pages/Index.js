3f28e1f5f29e90f8a1c39e5c7ea50bc43dcf227a
dennis <denizponce16@gmail.com>
2022-05-08 13:10:37 -0500

    se dio estilos css en index.module.css

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,46 +1,47 @@
-import React from 'react';
-import getDeviceStatus from '../functions/getDeviceStatus';
+import React from "react";
+import getDeviceStatus from "../functions/getDeviceStatus";
+import styles from "../styles/index.module.css";
 
 function Index() {
-	const [inputValue, setInputValue] = React.useState('');
-	const [estatus, setEstatus] = React.useState(undefined);
-	return (
-		<div
-			style={{
-				height: '100vh',
-				width: '100vw',
-				display: 'grid',
-				placeContent: 'center',
-			}}>
-			<h1>Estatus:</h1>
-			<div style={{ width: '50%', display: 'flex', margin: '0 auto' }}>
-				<input
-					type="text"
-					placeholder="Ingresa id o factura"
-					id="search-status"
-					value={inputValue}
-					onChange={e => setInputValue(e.target.value)}
-				/>
-				<button onClick={() => getDeviceStatus(inputValue).then(setEstatus)}>
-					Buscar
-				</button>
-			</div>
+  const [inputValue, setInputValue] = React.useState("");
+  const [estatus, setEstatus] = React.useState(undefined);
+  return (
+    <div className={styles.searchWrapper}>
+      <div className={styles.title}>
+        <h1>FIX MY PHONE</h1>
+        <h3>Estatus:</h3>
+      </div>
+      <div className={styles.buttonSearch}>
+        <input
+          type="text"
+          placeholder="Ingresa cedula o factura"
+          id="search-status"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button
+          className={styles.btn}
+          onClick={() => getDeviceStatus(inputValue).then(setEstatus)}
+        >
+          <i className="bi bi-search"></i>
+        </button>
+      </div>
 
-			{estatus === null && (
-				<p style={{ textAlign: 'center' }}>
-					No se encontró el dispositivo, pruebe con otra factura o id.
-				</p>
-			)}
-			{(estatus !== null) & (estatus !== undefined) ? (
-				<p>
-					{' '}
-					El <strong>{estatus.equipo}</strong> de{' '}
-					<strong> {estatus.propietario}</strong> se encuentra la fase:{' '}
-					<strong>{estatus.estado}</strong>
-				</p>
-			) : null}
-		</div>
-	);
+      {estatus === null && (
+        <p className={styles.messageError}>
+          No se encontró el dispositivo, pruebe con otra factura o id.
+        </p>
+      )}
+      {(estatus !== null) & (estatus !== undefined) ? (
+        <p className={styles.message}>
+          {" "}
+          El dispositivo <strong>{estatus.equipo}</strong> de{" "}
+          <strong> {estatus.propietario}</strong>, se encuentra en la fase:{" "}
+          <strong>{estatus.estado}</strong>
+        </p>
+      ) : null}
+    </div>
+  );
 }
 
 export default Index;
